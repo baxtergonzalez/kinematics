32ed499ec533bf7694362271c11582fe9146c945
Baxter <57194018+baxtergonzalez@users.noreply.github.com>
2021-12-10 15:05:27 -0600

    add visuals for sphere and circles
    visually display the range of b and platform

--- delta_kinematics_paper_reference.py
+++ delta_kinematics_paper_reference.py
@@ -1,4 +1,5 @@
 #referencing https://hypertriangle.com/~alex/delta-robot-tutorial/
+# and https://www.ohio.edu/mechanical-faculty/williams/html/PDF/DeltaKin.pdf 
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -54,24 +55,79 @@ def rotationMatrix(rAng):
 def drawVector(start, end, col='b'):
     ax.quiver(start[0], start[1], start[2], end[0], end[1], end[2], color=col)
 
+def drawSphere(origin, radius):
+    """
+    -draw a sphere around input position of radius cLength + dLength(linkage + platform length)
+    -represents all possible positions of platform
+    -intersection of this sphere with circles drawn around the start of linkage b is the endpoint of where the b linkage should be
+    """
+    u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
+    x = radius* np.cos(u)*np.sin(v) + origin[0]
+    y = radius* np.sin(u)*np.sin(v)+ origin[1]
+    z = radius* np.cos(v)+ origin[2]
+    ax.plot_wireframe(x, y, z, color="r")
+
+    coords = np.array([x,y,z])
+    return coords
+
+def drawCircle(origin, radius, phi, bMin = 0, bMax = 2*np.pi):
+    """
+    -draw a circle of radius bLength centered at the start of the b joint
+    -the circle is constrained to be co-planer with the b linkage
+    -this circle represents all of the possible positions for b
+    -can also restrict the circle into a segment to represent the range of motors
+    """
+    theta = np.linspace(bMin, bMax, 201)
+    x = radius*np.cos(theta)*np.cos(phi) + origin[0]
+    y = radius*np.cos(theta)*np.sin(phi) + origin[1]
+    z = radius*np.sin(theta) + origin[2]
+ 
+    ax.plot(x,y,z)
+
+    coords = np.array([x,y,z])
+    return coords
+
+def findClosestMatch(set1, set2):
+    closestSet = 0,0,0
+    return closestSet
 # position vector is the input position vector of the center of end effector
 # rotation is the rotation matrix for the orientation of the end effector (input in degrees)
 # origin is constant. always (0,0,0)
 
-position = makeVect([0,0,15])
+position = makeVect([0,0,20])
 rotation = rotationMatrix([0,0,0])
 origin = makeVect([0,0,0])
+cLength = 10
+bLength = 15
 
 # a vectors are first grounded linkage from origin
 
-aLength = 10
-a1 = makeVect([aLength, (360/3)*1, 0], cyl = True)
-a2 = makeVect([aLength, (360/3)*2, 0], cyl = True)
-a3 = makeVect([aLength, (360/3)*3, 0], cyl = True)
+aLength = 15
+a1 = makeVect([aLength, 0, 0], cyl = True)
+a2 = makeVect([aLength, (360/3)*1, 0], cyl = True)
+a3 = makeVect([aLength, (360/3)*-1, 0], cyl = True)
 
+# d vectors are platform vectors going from position to connect to legs
+
+dLength = 5
+d1 = np.dot(makeVect([dLength, 0, 0], cyl = True), rotation)
+d2 = np.dot(makeVect([dLength, (360/3)*1, 0], cyl = True), rotation)
+d3 = np.dot(makeVect([dLength, (360/3)*-1, 0], cyl = True), rotation)
+
+# draw static design vectors a 1-3 and d 1-3
 drawVector(origin, a1)
 drawVector(origin, a2)
 drawVector(origin, a3)
+
+drawVector(position, d1, 'g')
+drawVector(position, d2, 'g')
+drawVector(position, d3, 'g')
+
+drawSphere(position, cLength+dLength)
+print(drawSphere(position, cLength+dLength))
+drawCircle(a1, bLength, 0, np.pi/4, 3*np.pi/4)
+drawCircle(a2, bLength, 2*np.pi/3,  np.pi/4, 3*np.pi/4)
+drawCircle(a3, bLength, 2*np.pi/-3,  np.pi/4, 3*np.pi/4)
 
 # display plot
 # !!ALWAYS AT END OF SCRIPT!!
